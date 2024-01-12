742f8c6d2f1d2882c7dd40f33c2267d39020e5dd
ashfame <mail@ashfame.com>
2024-01-13 02:43:09 +0400

    update the script to correctly update the updated code in blueprint

--- chatrix/runPHPCode.php
+++ chatrix/runPHPCode.php
@@ -4,9 +4,11 @@ require_once 'wordpress/wp-load.php';
 
 $chatrix_block = '<!-- wp:automattic/chatrix {"instanceId":"2530674218113325","defaultHomeserver":"matrix.lol"} /-->';
 
-$sample_page = get_post( 2 );
+$sample_page_post_id = 2;
+
+$sample_page = get_post( $sample_page_post_id );
 
 wp_update_post( array(
-    'ID' => 2,
+    'ID' => $sample_page_post_id,
     'post_content' => $chatrix_block . '  ' . $sample_page->post_content
 ) );
--- chatrix/update_blueprint.sh
+++ chatrix/update_blueprint.sh
@@ -4,4 +4,7 @@ if ! command -v jq &> /dev/null ; then
     echo "jq is not installed. Please install jq (https://jqlang.github.io/jq/) before running this script."
 fi
 
-jq --arg newCode "$(cat runPHPCode.php)" '.steps[] |= if .step == "runPHP" then .code = $newCode else . end' blueprint.json > blueprint.json
+jq --arg newCode "$(cat runPHPCode.php)" '.steps[] |= if .step == "runPHP" then .code = $newCode else . end' blueprint.json > new_blueprint.json
+
+rm blueprint.json
+mv new_blueprint.json blueprint.json
